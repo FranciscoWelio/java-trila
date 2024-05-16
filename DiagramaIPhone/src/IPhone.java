aa570341b0719903d546f1156d01433af7737ac1
Francisco Welio <fwelio.dev@gmail.com>
2024-05-16 17:05:27 -0300

    Desafio um Iphone

---
+++ DiagramaIPhone/src/IPhone.java
@@ -0,0 +1,36 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    
+    public void tocar(){
+
+    }
+    public void pausar(){
+
+    }
+    public void selecionarMusica(String musica){
+
+    }
+    public void ligar(String numero){
+
+    }
+    public void atender(){
+
+    }
+    public void iniciarCorreioVoz(){
+
+    }
+    public void exibirPagina(String url){
+
+    }
+    public void adicionarNovaAba(){
+
+    }
+    public void atualizarPagina(){
+
+    }
+    public static void main(String[] args) {
+        IPhone telefone = new IPhone();
+        telefone.tocar();
+        telefone.selecionarMusica("Oceano");
+        telefone.atualizarPagina();
+    }
+}
